0132dfee09e67a1ae340bdf792ad4524af2f17c8
ignacywnuk <ignacy.wnuk@gmail.com>
2023-05-31 12:20:22 +0200

    file adjustment

--- XML_CS/Results/main.cs
+++
@@ -1,4 +0,0 @@
-Root root = new Root();
-Mushroom_picker mushroom_picker = new Mushroom_picker("Ignacy", "100");
-Mushrooms mushrooms = new Mushrooms();
-Mushroom mushroom = new Mushroom("Borownik", "yes");
